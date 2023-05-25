939fe980efe2929cc71350576eb38885b8f1f98f
Aden Herold <aden.herold1@gmail.com>
2023-05-25 14:55:53 +1000

    feat: implemented basic echo server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,2 +1,28 @@
+use std::env;
+use tokio::net::{TcpListener, TcpStream};
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let addr = env::args()
+        .skip(1)
+        .next()
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    let listener = TcpListener::bind(&addr).await.unwrap();
+
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            process_connection(&mut socket).await;
+        });
+    }
+}
+
+async fn process_connection(socket: &mut TcpStream) {
+    let (mut rd, mut wr) = socket.split();
+
+    loop {
+        tokio::io::copy(&mut rd, &mut wr).await.unwrap();
+    }
+}
