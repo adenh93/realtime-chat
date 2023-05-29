24c523a913b4dd6b0881509a01f74d57fca0b654
Aden Herold <aden.herold1@gmail.com>
2023-05-29 19:15:42 +1000

    test: added unit tests for try_pop_arg utility function

--- src/utils.rs
+++ src/utils.rs
@@ -11,3 +11,31 @@ pub fn try_pop_arg(args: &mut Iter<&str>, name: &str) -> Result<String, CommandE
 
     Ok(arg)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use fake::{faker::lorem::en::Word, Fake};
+
+    #[test]
+    fn successfully_pops_arg() {
+        let name = Word().fake();
+        let args = vec![name];
+        let mut iter = args.iter();
+
+        let result = try_pop_arg(&mut iter, name);
+
+        assert_eq!(result, Ok(name.into()));
+    }
+
+    #[test]
+    fn returns_error_if_no_arg() {
+        let args = vec![];
+        let mut iter = args.iter();
+
+        let name = Word().fake();
+        let result = try_pop_arg(&mut iter, name);
+
+        assert_eq!(result, Err(CommandError::MissingArgument(name.into())))
+    }
+}
