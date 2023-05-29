d781e3ec8846defb534fe376c78ef8b9c4bcf8c0
Aden Herold <aden.herold1@gmail.com>
2023-05-29 20:20:48 +1000

    test: added unit tests to TryFrom implementation for Me command

--- src/commands/me.rs
+++ src/commands/me.rs
@@ -6,6 +6,12 @@ pub struct Me {
     message: String,
 }
 
+impl Me {
+    pub fn new(message: String) -> Self {
+        Self { message }
+    }
+}
+
 #[async_trait]
 impl CommandApply for Me {
     async fn apply(&self, conn: &mut Connection) -> Result<(), CommandError> {
@@ -29,5 +35,46 @@ impl TryFrom<Vec<&str>> for Me {
         let message = args.join(" ");
 
         Ok(Self { message })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use fake::{
+        faker::lorem::en::{Sentence, Word},
+        Fake,
+    };
+
+    #[test]
+    fn parses_me_command() {
+        let word: &str = &Word().fake::<String>();
+        let args = vec![word];
+
+        let command = Me::try_from(args);
+        let expected = Me::new(word.into());
+
+        assert_eq!(command, Ok(expected));
+    }
+
+    #[test]
+    fn parses_whitespace_delimited_sentence() {
+        let sentence: &str = &Sentence(0..2).fake::<String>();
+        let args = vec![sentence];
+
+        let command = Me::try_from(args);
+        let expected = Me::new(sentence.into());
+
+        assert_eq!(command, Ok(expected));
+    }
+
+    #[test]
+    fn returns_error_if_missing_message_argument() {
+        let args = vec![];
+
+        let command = Me::try_from(args);
+        let expected = CommandError::MissingArgument("message".into());
+
+        assert_eq!(command, Err(expected));
     }
 }
