04e4b69aa7c472b484a73bf3a65382f98a292f91
Aden Herold <aden.herold1@gmail.com>
2023-05-30 22:26:34 +1000

    test: added unit tests for frame.rs module

--- src/frame.rs
+++ src/frame.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Frame {
     Message(String),
     ServerMessage(String),
@@ -38,5 +38,89 @@ impl Frame {
                 format!("Invalid message frame"),
             ))?,
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use fake::{faker::lorem::en::Word, Fake};
+
+    #[test]
+    fn frame_format_returns_message_format() {
+        let message = Word().fake::<String>();
+        let length = message.len();
+
+        let frame = Frame::Message(message.clone());
+        let format = frame.frame_format();
+
+        assert_eq!(format, (b'+', message, length));
+    }
+
+    #[test]
+    fn frame_format_returns_server_message_format() {
+        let message = Word().fake::<String>();
+        let length = message.len();
+
+        let frame = Frame::ServerMessage(message.clone());
+        let format = frame.frame_format();
+
+        assert_eq!(format, (b'$', message, length));
+    }
+
+    #[test]
+    fn frame_format_returns_error_format() {
+        let message = Word().fake::<String>();
+        let length = message.len();
+
+        let frame = Frame::Error(message.clone());
+        let format = frame.frame_format();
+
+        assert_eq!(format, (b'-', message, length));
+    }
+
+    #[test]
+    fn message_extracts_frame_message() {
+        let message = Word().fake::<String>();
+
+        let message_frame = Frame::Message(message.clone());
+        let server_message_frame = Frame::ServerMessage(message.clone());
+        let error_frame = Frame::Error(message.clone());
+
+        assert_eq!(message_frame.message(), message);
+        assert_eq!(server_message_frame.message(), message);
+        assert_eq!(error_frame.message(), message);
+    }
+
+    #[test]
+    fn try_from_prefix_retrieves_message() {
+        let message = Word().fake::<String>();
+        let frame = Frame::try_from_prefix('+', &message).unwrap();
+
+        assert_eq!(frame, Frame::Message(message));
+    }
+
+    #[test]
+    fn try_from_prefix_retrieves_server_message() {
+        let message = Word().fake::<String>();
+        let frame = Frame::try_from_prefix('$', &message).unwrap();
+
+        assert_eq!(frame, Frame::ServerMessage(message));
+    }
+
+    #[test]
+    fn try_from_prefix_retrieves_error() {
+        let message = Word().fake::<String>();
+        let frame = Frame::try_from_prefix('-', &message).unwrap();
+
+        assert_eq!(frame, Frame::Error(message));
+    }
+
+    #[test]
+    fn try_from_prefix_returns_error_if_unknown_prefix() {
+        let message = Word().fake::<String>();
+        let frame = Frame::try_from_prefix('\0', &message);
+
+        assert!(frame.is_err());
     }
 }
